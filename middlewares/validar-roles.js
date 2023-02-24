b82948e13a75010b0a170135c10ca1422cb4b75f
Fabian Herrera <jherrera-2018183@kinal.edu.gt>
2023-02-23 20:57:54 -0600

    primer commit

---
+++ middlewares/validar-roles.js
@@ -0,0 +1,52 @@
+const { request, response } = require('express');
+
+const esAdminRole = (req = request, res = response, next) => {
+
+    if ( !req.usuario ) {
+        return res.status(500).json({
+            msg: 'Se require verificar el role sin validar el token primero'
+        });
+    }
+
+    const { rol, nombre } = req.usuario;
+
+    if ( rol !== 'ADMIN_ROLE' ) {
+        return res.status(500).json({
+            msg: `${ nombre } no es Administrador - No tiene acceso a esta función`
+        });
+    }
+
+    next();
+
+
+}
+
+
+const tieneRole = ( ...roles ) => {
+
+    return (req = request, res= response, next) => {
+
+        if (!req.usuario) {
+            return res.status(500).json({
+                msg: 'Se quiere verificar el role sin validar el token primero'
+            })
+        }
+
+        if (!roles.includes( req.usuario.rol)) {
+            return res.status(401).json({
+                msg: `El servicio requiere uno de estos roles: ${ roles }`
+            })
+
+        }
+
+        next();
+
+    }
+
+}
+
+
+module.exports = {
+    tieneRole,
+    esAdminRole
+}
